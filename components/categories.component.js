746c6560d7492e0f049d2ea201f9cac940214b4f
Higgor Calheiros Pereira Pinto <higgor_calheiros@bb.com.br>
2022-05-16 07:45:15 -0300

    Commit inicial

---
+++ components/categories.component.js
@@ -0,0 +1,9 @@
+(function () {
+  "use strict";
+  angular.module("MenuApp").component("categories", {
+    templateUrl: "templates/categories.component.html",
+    bindings: {
+      categories: "<",
+    },
+  });
+})();
---
+++ components/items.component.js
@@ -0,0 +1,9 @@
+(function () {
+  "use strict";
+  angular.module("MenuApp").component("items", {
+    templateUrl: "templates/items.component.html",
+    bindings: {
+      items: "<",
+    },
+  });
+})();
